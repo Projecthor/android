fbd5f2e949590e8db1911f5fa166b36df01fe0bc
lucas8 <luc.linux@mailoo.org>
2013-01-14 17:15:18 +0100

    Ajout d'un serveur de test bluetooth basique.

---
+++ test/server.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2.7 
+# -*-coding:Utf-8 -*
+
+from bluetooth import *
+import random as rd
+
+server_sock = BluetoothSocket(RFCOMM)
+server_sock.bind(("", PORT_ANY))
+server_sock.listen(1)
+
+advertise_service(server_sock, "RoboTXBTController", service_classes = [SERIAL_PORT_CLASS], profiles = [SERIAL_PORT_PROFILE])
+
+client_sock, client_info = server_sock.accept()
+print "Connexion de", client_info
+
+while True :
+	data = client_sock.recv(1024)
+	print data 
+
+client_sock.close()
+server_sock.close()

3312d7172f0feb487c39142e01ecbe65326f1304
Champitoad <pablo.donato@mailoo.org>
2013-01-23 18:22:46 +0100

    Utilisation du nom Projecthor dans le serveur bluetooth.

--- test/server.py
+++ test/server.py
@@ -8,7 +8,7 @@ server_sock = BluetoothSocket(RFCOMM)
 server_sock.bind(("", PORT_ANY))
 server_sock.listen(1)
 
-advertise_service(server_sock, "RoboTXBTController", service_classes = [SERIAL_PORT_CLASS], profiles = [SERIAL_PORT_PROFILE])
+advertise_service(server_sock, "Projecthor", service_classes = [SERIAL_PORT_CLASS], profiles = [SERIAL_PORT_PROFILE])
 
 client_sock, client_info = server_sock.accept()
 print "Connexion de", client_info
@@ -17,7 +17,7 @@ while True :
 	data = client_sock.recv(1024)
 	print data
 	if data == "compute" :
-		print "Prêt"
+		print "ready"
 		client_sock.sendall("ready")
 
 client_sock.close()
